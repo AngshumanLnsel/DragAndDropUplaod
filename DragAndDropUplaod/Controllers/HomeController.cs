4bf8ec13203d356aa782aad838e4b98083cab72b
AngshumanLnsel <angshuman.lnsel@gmail.com>
2022-10-29 16:23:20 +0530

    Add project files.

---
+++ DragAndDropUplaod/Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using DragAndDropUplaod.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragAndDropUplaod.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
+        {
+            _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //[HttpGet]
+        //public ActionResult Upload()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        public async Task<ActionResult> UploadAsync(ICollection<IFormFile> files)
+        {
+            bool isSavedSuccessfully = true;
+            string fName = "testing";
+            try
+            {
+
+                foreach (IFormFile file in files)
+                {
+                    //HttpPostedFileBase file = Request.Files[fileName];
+                    fName = file.FileName;
+                    if (file != null && file.Length > 0)
+                    {
+                        string webRootPath = _webHostEnvironment.WebRootPath;
+                        var path = Path.Combine(webRootPath);
+                        string pathString = System.IO.Path.Combine(path.ToString());
+                        var fileName1 = Path.GetFileName(file.FileName);
+                        bool isExists = System.IO.Directory.Exists(pathString);
+                        if (!isExists) System.IO.Directory.CreateDirectory(pathString);
+                        //var uploadpath = string.Format("{0}\\{1}", pathString, file.FileName);
+                        using (var fileStream = new FileStream(Path.Combine(pathString, file.FileName), FileMode.Create))
+                        {
+                            await file.CopyToAsync(fileStream);
+                        }
+                        //file.SaveAsAsync(uploadpath);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                isSavedSuccessfully = false;
+            }
+            if (isSavedSuccessfully)
+            {
+                return Json(new
+                {
+                    Message = fName
+                });
+            }
+            else
+            {
+                return Json(new
+                {
+                    Message = "Error in saving file"
+                });
+            }
+        }
+
+        public IActionResult Calender()
+        {
+            return View();
+        }
+    }
+}
